e1abba7d9b978f60c4616386b904d5219342615a
Kacper Popczyński <popkacper@gmail.com>
2021-09-02 22:05:52 +0200

    Use 1 array to store todos

--- src/app/dashboard/feature/dashboard/dashboard.component.ts
+++ src/app/dashboard/feature/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { ITodo } from '@app/core/domain/interfaces/todo.interface';
-import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TodoFacade } from '@app/dashboard/domain/services/todo-facade/todo-facade.service';
 import { ITodoListGroup } from '@app/dashboard/domain/interfaces/todo-list-group.interface';
@@ -24,17 +24,28 @@ export class DashboardComponent implements OnInit {
 
     public isFetchTodosPending$: Observable<boolean> = this._todoFacade.isFetchTodosPending$;
 
-    public fakeTodoGroups$: Observable<ITodoListGroup[]> = of(new Array(3).fill({
-        date: new Date(1900, 1, 1).toISOString(),
-        todos: new Array(2).fill(undefined),
-    } as ITodoListGroup));
+    public fakeTodoGroups$: Observable<ITodoListGroup[]> = this.isFetchTodosPending$.pipe(
+        map(isFetchTodosPending => isFetchTodosPending ? new Array(3).fill({
+            date: new Date(1900, 1, 1).toISOString(),
+            todos: new Array(2).fill(undefined),
+        } as ITodoListGroup) : []),
+    );
 
     public todoGroups$: Observable<ITodoListGroup[]> = combineLatest([
         this._todoFacade.todos$,
+        this.isFetchTodosPending$,
         this.showCompletedTodos$,
     ]).pipe(
-        map(([ todos, showCompleted ]) => todos.filter(todo => todo.isDone === showCompleted)),
-        map(todos => this._todoFacade.groupTodosByDate(todos)),
+        map(([ todos, isFetchTodosPending, showCompleted ]) => {
+            const groupedTodos: any[] = this._todoFacade.groupTodosByDate(
+                todos.filter(todo => todo.isDone === showCompleted)
+            );
+
+            return isFetchTodosPending ? new Array(3).fill({
+                date: new Date(1900, 1, 1).toISOString(),
+                todos: new Array(3).fill(undefined),
+            } as ITodoListGroup) : groupedTodos;
+        }),
     );
 
     public constructor(
